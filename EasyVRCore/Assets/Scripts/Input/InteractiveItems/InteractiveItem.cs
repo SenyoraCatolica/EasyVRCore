dee19b515ac5496f8657c56ff2e56da32fd19362
miquelgiru <miquel_giru_96@hotmail.com>
2019-02-16 16:21:31 +0100

    Starting interactive item system

--- EasyVRCore/Assets/Scripts/Input/InteractiveItems/InteractiveItem.cs
+++ EasyVRCore/Assets/Scripts/Input/InteractiveItems/InteractiveItem.cs
@@ -3,17 +3,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractiveItem : MonoBehaviour, IInteractiveItem
+
+[System.Serializable]
+public struct InteractiveItemConfig
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool BlocksInput;
+    public bool Autoselect;
+    public bool AllowRepeatSelection;
+
+    public bool StaysSelected;
+    private bool m_isSelected_;
+
+    public bool IsSelected()
     {
-        
+        return m_isSelected_;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setIsSelected(bool isSelected)
     {
-        
+        m_isSelected_ = isSelected;
+    }
+}
+
+public abstract class InteractiveItem : MonoBehaviour, IInteractiveItem
+{
+    [SerializeField] protected InteractiveItemConfig m_config;
+    [SerializeField] protected bool m_enabled = false;
+    protected bool m_alreadySelect = false;
+
+    public abstract void Enter();
+    public abstract void Exit();
+    public abstract void Hover();
+    public abstract void Select();
+
+    public bool IsAutoselect()
+    {
+        return m_config.Autoselect;
+    }
+
+    public bool IsRepeatSelection()
+    {
+        return m_config.AllowRepeatSelection;
+    }
+
+    public bool IsSelectable()
+    {
+        return !m_alreadySelect && m_enabled;
+    }
+
+    public bool IsSelected()
+    {
+        return m_config.StaysSelected;
+    }
+
+    public void SetSelected(bool isSelected)
+    {
+        m_config.setIsSelected(isSelected);
+    }
+
+    public bool StaysSelected()
+    {
+        return m_config.StaysSelected;
     }
 }
